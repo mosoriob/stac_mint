1f25ffe5263513a0f8dbaa76480516c55569bd30
Maximiliano Osorio <mosorio@isi.edu>
2020-09-03 12:07:30 -0700

    fix: adding examples

---
+++ create_items.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Content-Type: application/json" -d@examples/usgs_water_data_ca.json http://d.mint.isi.edu/collections
+curl -X POST -H "Content-Type: application/json" -d@examples/usgs_water_data_ca.json http://d.mint.isi.edu/collections/usgs_water_ca
+curl -X POST -H "Content-Type: application/json" -d@examples/feature.json http://d.mint.isi.edu/collections/usgs_water_ca/items/90c8606b-f878-45a0-828f-31b547cb1bd7
